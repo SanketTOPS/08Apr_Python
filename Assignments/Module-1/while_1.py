77c38201632a8e1e4a289bd721822f182c332d42
SanketTops <sanket.tops@gmail.com>
2026-04-27 10:58:50 +0530

    Loops

---
+++ Assignments/Module-1/while_1.py
@@ -0,0 +1,6 @@
+i=1 #init.
+
+while i<=10: #condition - TRUE
+    print(i)
+    i+=1 #i=i+1
+    
---
+++ Assignments/Module-1/while_table.py
@@ -0,0 +1,6 @@
+i=1
+
+n=int(input("Enter any number:"))
+while i<=10:
+    print(f"{n} * {i} = {n*i}")
+    i+=1
