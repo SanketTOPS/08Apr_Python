311bcbb9b4e4dc25f7694f8385d1499c3f4cdc64
SanketTops <sanket.tops@gmail.com>
2026-04-29 11:00:51 +0530

    loops

---
+++ Assignments/Module-1/pattern_1.py
@@ -0,0 +1,4 @@
+for i in range(1,6): #row
+    for j in range(i):
+        print("*",end=" ")
+    print(" ") #row break
---
+++ Assignments/Module-1/pattern_2.py
@@ -0,0 +1,4 @@
+for i in range(1,6): #row
+    for j in range(i):
+        print(i,end=" ")
+    print(" ") #row break
