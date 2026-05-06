a96095ad43d4e494edf5f25d07f1c13f17e54534
SanketTops <sanket.tops@gmail.com>
2026-05-06 10:55:22 +0530

    Collections

---
+++ Assignments/Module-2_Collection/set_example.py
@@ -0,0 +1,37 @@
+myset={'a','b','c','d','e','f'}
+"""print(myset)
+print(len(myset))"""
+
+# ------------------ #
+"""print(myset)
+if 'c' in myset:
+    print("Yes...")
+else:
+    print("Noo")"""
+# ------------------ #
+#print(myset)
+
+"""for i in myset:
+    print(i)"""
+    
+# ----------------- #
+#print(myset)
+#myset.add("g")
+#myset.update(['h','i','j'])
+#myset.remove('f')
+#myset.pop()
+#myset.clear()
+#del myset
+#print(myset)
+
+# ------------------ #
+
+print(myset)
+newset={'p','q','r','s','a','c'}
+print(newset)
+
+"""x=myset.union(newset)
+print(x)"""
+
+x=myset.intersection(newset)
+print(x)
---
+++ Assignments/Module-2_Collection/tuple_example.py
@@ -0,0 +1,25 @@
+data=('ahmedabad','rajkot','baroda','surat','gandhinagar')
+"""print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3]) #slice"""
+
+# ----------------- #
+"""print(data)
+print(len(data))"""
+# ----------------- #
+
+"""print(data)
+if 'surat' in data:
+    print("Yes...")
+else:
+    print("Noo")"""
+    
+# ----------------- #
+"""print(data)
+for i in data:
+    print(i)"""
+    
+# ----------------- #
+del data
+print(data)
