82b5660723448be75b81df08e34092902f3cb3bf
SanketTops <sanket.tops@gmail.com>
2026-05-08 10:55:44 +0530

    Dict

---
+++ Assignments/Module-2_Collection/dict_example.py
@@ -0,0 +1,58 @@
+stdata={'id':101,'name':'sanket','city':'rajkot'}
+
+"""print(stdata)
+print(stdata['name'])
+print(stdata.get('city'))
+print(stdata.keys())
+print(stdata.values())
+print(len(stdata))"""
+
+# -------------------- #
+#print(stdata)
+
+"""if 'name' in stdata:
+    print("Yes...")
+else:
+    print("No...")"""
+    
+"""if 'sanket' in stdata.values():
+    print("Yes...")
+else:
+    print("No...")"""
+    
+# ------------------- #
+
+#print(stdata)
+"""for i in stdata:
+    print(i)"""
+    
+"""for i in stdata.values():
+    print(i)"""
+    
+"""for i in stdata.items():
+    print(i)"""
+
+"""for i,j in stdata.items():
+    print(i,j)"""
+
+"""for i,j in stdata.items():
+    print(f"Key={i} and Value={j}")"""
+
+"""print(stdata)
+
+stdata['id']=102 #value change
+print(stdata)"""
+
+
+# ------------------------ #
+print(stdata)
+#stdata["sub"]='python' #add new a pair
+#stdata.pop('city')
+#stdata.clear()
+#del stdata['id']
+#print(stdata)
+
+#newstdata=stdata.copy()
+#print(newstdata)
+
+# ----------------------- #
---
+++ Assignments/Module-2_Collection/nested_dict_2.py
@@ -0,0 +1,8 @@
+stdata=[
+    {'id':101,'name':'sanket'},
+    {'id':102,'name':'nirav'},
+    {'id':103,'name':'dipak'},
+    {'id':104,'name':'darshan'},
+    {'id':105,'name':'harsh'}
+]
+print(stdata[0])
